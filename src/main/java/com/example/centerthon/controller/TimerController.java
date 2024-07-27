c6e66efdb7272b882a12ec76bd5c80b29f2423b4
mrri-18 <emk0528@swu.ac.kr>
2024-07-27 22:37:03 +0900

    타이머 완료, 팝업 미완

---
+++ src/main/java/com/example/centerthon/controller/TimerController.java
@@ -0,0 +1,19 @@
+package com.example.centerthon.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequiredArgsConstructor
+public class TimerController {
+
+    @GetMapping("/timer")
+    public String timer(@RequestParam(value = "time", defaultValue = "10") Integer time, Model model) {
+        model.addAttribute("initialTime", time); // 사용자로부터 받은 시간 또는 기본값으로 설정
+        return "timer/timer";
+    }
+}
+
