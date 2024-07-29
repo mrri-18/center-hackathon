cf51cbae06c2316c891f92d5921ebe5660f85581
mrri-18 <emk0528@swu.ac.kr>
2024-07-29 16:08:40 +0900

    영상 임베딩 완료

--- src/main/java/com/example/centerthon/controller/UserController.java
+++ src/main/java/com/example/centerthon/controller/UserController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.HashMap;
 import java.util.Map;
 
-@RequestMapping("")
+
 @Controller
 @RequiredArgsConstructor
 public class UserController {
