628a1c3ef6c03121927270efa3b98340052b7c9b
mrri-18 <emk0528@swu.ac.kr>
2024-07-29 12:43:52 +0900

    팝업 완료 영상 임베딩 미완

--- src/main/java/com/example/centerthon/controller/TimerController.java
+++ src/main/java/com/example/centerthon/controller/TimerController.java
@@ -1,11 +1,9 @@
 package com.example.centerthon.controller;
-
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
 @Controller
 @RequiredArgsConstructor
 public class TimerController {
